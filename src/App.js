6c46f74eedcd357210348c3fd6359cd1d79e0a67
Mark Lynskey <mlynskey2@gmail.com>
2022-01-25 22:46:54 +1300

    feat: add basic routes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,24 @@
-const App = () => <p>Hello world</p>;
+import { Routes, Route, Link } from "react-router-dom";
+
+const App = () => (
+  <>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+    </Routes>
+  </>
+);
+
+const Home = () => <h1>Home</h1>;
+
+const About = () => <h1>About</h1>;
+
+const Contact = () => <h1>Contact</h1>;
 
 export default App;
